a5abb8059367e956f6504837853dce213a8541bd
Murat <muha188@mail.ru>
2019-08-30 16:51:33 +0600

    Modified group

--- src/com/company/model/Group.java
+++ src/com/company/model/Group.java
@@ -8,4 +8,44 @@ public class Group {
     private int teacherId;
     private Date startDate;
 
+    public Group(int id, int courseId, int teacherId, Date startDate) {
+        this.id = id;
+        this.courseId = courseId;
+        this.teacherId = teacherId;
+        this.startDate = startDate;
+    }
+
+    public Group(int courseId, int teacherId, Date startDate) {
+        this.courseId = courseId;
+        this.teacherId = teacherId;
+        this.startDate = startDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
 }
--- src/com/company/services/AdministratorService.java
+++ src/com/company/services/AdministratorService.java
@@ -1,7 +1,9 @@
 package com.company.services;
 
+import com.company.model.Group;
 import com.company.model.Student;
 
 public interface AdministratorService {
-    void registerStudent(Student student)
+    void registerStudent(Student student, Group group);
+    void unRegisterStudent(Student student, Group group);
 }
